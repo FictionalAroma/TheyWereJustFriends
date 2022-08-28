1abfc4d00a0b44e139e91f13816f28bf4163d4fe
Fictional <fictionalaroma@gmail.com>
2022-08-28 19:48:00 +0100

    basic jump physics and rigid body conversion

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,6 +14,7 @@ public class PlayerController : MonoBehaviour, PlayerInputControls.IPlayerAction
     #region UnityFields
 
     [SerializeField] private float movementFactor;
+    [SerializeField] private float jumpForce;
 
     #endregion
 
@@ -24,16 +25,34 @@ public class PlayerController : MonoBehaviour, PlayerInputControls.IPlayerAction
     private InputAction _move;
     private InputAction _fire;
     private Animator _animator;
+    private Rigidbody2D _playerBody;
 
     #endregion
 
     private Vector2 _currentMoveInputVector;
+    private InputAction _moveAction;
+    private InputAction _jumpAction;
 
 
     private void Awake()
     {
         _controls = new PlayerInputControls();
         _animator = GetComponent<Animator>();
+        _playerBody = GetComponent<Rigidbody2D>();
+
+        SetControlCallbacks();
+
+    }
+
+    private void SetControlCallbacks()
+    {
+        _moveAction = _controls.Player.Move;
+        _moveAction.started += OnMove;
+        _moveAction.canceled += OnMove;
+
+        _jumpAction = _controls.Player.Jump;
+        _jumpAction.canceled += OnJump;
+        //_jumpAction.performed += OnJump;
 
     }
 
@@ -44,27 +63,30 @@ public class PlayerController : MonoBehaviour, PlayerInputControls.IPlayerAction
 
     private void EnableControls()
     {
-        _controls.Player.SetCallbacks(this);
         _controls.Enable();
         _controls.Player.Enable();
     }
 
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        Vector2 movementVector = _currentMoveInputVector * (Time.deltaTime * movementFactor);
+        float movementVectorX = _currentMoveInputVector.x * movementFactor;
 
-        var playerXMovementAbs = Mathf.Abs(movementVector.x);
+        var playerXMovementAbs = Mathf.Abs(movementVectorX);
 
-        transform.Translate(movementVector);
+        var velTemp = _playerBody.velocity;
+
+        velTemp.x = movementVectorX;
+        _playerBody.velocity = velTemp;
+
         _animator.SetFloat(PlayerMoveX, playerXMovementAbs);
-        _animator.SetFloat(PlayerMoveY, movementVector.y);
+        _animator.SetFloat(PlayerMoveY, _playerBody.velocity.y);
 
         if (playerXMovementAbs > Mathf.Epsilon)
         {
             var temp = transform.localScale;
-            temp.x = movementVector.x > 0 ? 1f : -1f;
+            temp.x = movementVectorX > 0 ? 1f : -1f;
             transform.localScale = temp;
         }
 
@@ -75,7 +97,10 @@ public class PlayerController : MonoBehaviour, PlayerInputControls.IPlayerAction
         _currentMoveInputVector = !context.canceled ? context.ReadValue<Vector2>() : Vector2.zero;
     }
 
-    public void OnLook(InputAction.CallbackContext context) {  }
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        _playerBody.velocity += new Vector2(_playerBody.velocity.x, jumpForce);
+    }
 
     public void OnFire(InputAction.CallbackContext context) { }
 
